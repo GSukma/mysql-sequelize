735ecf87e3f9260ee9147a77edd413de3b5b1a51
Gilang Sukma W <gilangsukma.wijaya@gmail.com>
2021-07-16 19:30:01 +0700

    initial file

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const errorHandler = require("./middlewares/errorHandler");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//routes
+const goodsRouter = require("./routes/good");
+const suppliersRouter = require("./routes/supplier");
+const customersRouter = require("./routes/customers");
+
+app.use("/goods", goodsRouter);
+app.use("/suppliers", suppliersRouter);
+app.use("/customers", customersRouter);
+
+app.use(errorHandler);
+
+const port = 5000;
+app.listen(port, () => console.log(`Server run on port : ${port}`));
---
+++ models/supplier.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class supplier extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      models.supplier.hasMany(models.good, {
+        foreignKey: "id_good",
+      });
+    }
+  }
+  supplier.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      paranoid: true, // enables deletedAt (soft delete)
+      timestamps: true, // enables createdAt and updatedAt
+      modelName: "supplier",
+    }
+  );
+  return supplier;
+};
